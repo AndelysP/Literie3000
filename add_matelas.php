b61409d8758ce76e3536638f5ca315f4035adec8
Andelys Pluquin <andelyst@hotmail.fr>
2023-02-27 16:48:32 +0100

    :art: Mise en place du responsive

--- add_matelas.php
+++ add_matelas.php
@@ -1,14 +1,11 @@
 <?php
 if (!empty($_POST)) {
-
     if (!empty($_POST["picture_upload"])) {
         $picture_upload = trim(strip_tags($_POST["picture_upload"]));
     };
-
     if (!empty($_POST["picture_link"])) {
         $picture_link = trim(strip_tags($_POST["picture_link"]));
     };
-
     $brand = trim(strip_tags($_POST["brand"]));
     $name = trim(strip_tags($_POST["name"]));
     $selectSize = trim(strip_tags($_POST["selectSize"]));
@@ -30,7 +27,6 @@ if (!empty($_POST)) {
     if ($price < 0) {
         $errors["price"] = "Le prix ne peut pas être inférieur à 0";
     }
-
     if ($newPrice < 0) {
         $errors["newPrice"] = "Le prix ne peut pas être inférieur à 0";
     }
@@ -41,12 +37,10 @@ if (!empty($_POST)) {
         $fileTmPath = $_FILES["picture_upload"]["tmp_name"];
         $fileName = $_FILES["picture_upload"]["name"];
         $fileType = $_FILES["picture_upload"]["type"];
-
         $fileNameArray = explode(".", $fileName);
         $fileExtension = end($fileNameArray);
         $newFileName = md5($fileName . time()) . "." . $fileExtension;
         $fileDestPath = "./assets/img/matelas/{$newFileName}";
-
         $allowedTypes = array("image/jpeg", "image/png", "image/webp");
         if (in_array($fileType, $allowedTypes)) {
             move_uploaded_file($fileTmPath, $fileDestPath);
@@ -54,7 +48,6 @@ if (!empty($_POST)) {
             $errors["picture_upload"] = "Le type de fichier est incorrect .jpg, .png ou .webp requis";
         }
     }
-
     if (!$errors) {
 
         $db = new PDO('mysql:host=localhost;dbname=literie3000;charset=UTF8', 'root', '');
@@ -71,28 +64,27 @@ if (!empty($_POST)) {
             $query->bindParam(":picture_link", $picture_link, PDO::PARAM_STR);
         }
 
+
         $query->bindParam(":brand", $brand, PDO::PARAM_STR);
         $query->bindParam(":name", $name, PDO::PARAM_STR);
         $query->bindParam(":size", $selectSize, PDO::PARAM_STR);
         $query->bindParam(":price", $price, PDO::PARAM_INT);
         $query->bindParam(":newPrice", $newPrice, PDO::PARAM_INT);
+
         $query->execute();
+        header("Location: index.php");
     }
 }
 
-header("Location: index.php");
 include("templates/header.php");
 ?>
-
 <main>
     <h1>Ajouter un matelas</h1>
-
     <div class="form-data">
         <form action="" method="POST" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="picture_upload">Image à uploader :</label>
                 <input type="file" name="picture_upload" id="picture_upload" value="<?= isset($picture_upload) ? $picture_upload : "" ?>">
-
                 <?php
                 if (isset($errors["picture_upload"])) {
                 ?>
@@ -101,18 +93,14 @@ include("templates/header.php");
                 }
                 ?>
             </div>
-
             <p>ou</p>
-
             <div class="form-group">
                 <label for="picture_link">Lien vers l'image :</label>
                 <input type="text" name="picture_link" id="picture_link" placeholder="http://exemple.com/image.jpg">
             </div>
-
             <div class="form-group">
                 <label for="brand"></label>
                 <input type="text" name="brand" id="brand" placeholder="Marque du matelas" value="<?= isset($brand) ? $brand : "" ?>">
-
                 <?php
                 if (isset($errors["brand"])) {
                 ?>
@@ -121,11 +109,9 @@ include("templates/header.php");
                 }
                 ?>
             </div>
-
             <div class="form-group">
                 <label for="name"></label>
                 <input type="text" name="name" id="name" placeholder="Nom du matelas" value="<?= isset($name) ? $name : "" ?>">
-
                 <?php
                 if (isset($errors["name"])) {
                 ?>
@@ -148,7 +134,6 @@ include("templates/header.php");
                     <option value="180x200">180x200</option>
                     <option value="200x200">200x200</option>
                 </select>
-
                 <?php
                 if (isset($errors["selectSize"])) {
                 ?>
@@ -157,11 +142,9 @@ include("templates/header.php");
                 }
                 ?>
             </div>
-
             <div class="form-group">
                 <label for="price"></label>
                 <input type="number" id="price" name="price" placeholder="Prix du matelas" value="<?= isset($price) ? $price : "" ?>" min="0">
-
                 <?php
                 if (isset($errors["price"])) {
                 ?>
@@ -170,11 +153,9 @@ include("templates/header.php");
                 }
                 ?>
             </div>
-
             <div class="form-group">
                 <label for="newPrice"></label>
                 <input type="number" id="newPrice" name="newPrice" placeholder="Prix soldé" value="<?= isset($newPrice) ? $newPrice : "" ?>" min="0">
-
                 <?php
                 if (isset($errors["newPrice"])) {
                 ?>
@@ -183,12 +164,10 @@ include("templates/header.php");
                 }
                 ?>
             </div>
-
             <input type="submit" value="Ajouter le matelas" class="btn-edit">
         </form>
     </div>
 </main>
-
 <?php
 include("templates/footer.php");
 ?>
